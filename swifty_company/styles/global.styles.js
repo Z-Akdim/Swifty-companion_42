283c26e869ed75602c61f716cedb37100d18b4f8
Z-Akdim <zakaria.akdim37@gmail.com>
2023-06-19 20:23:15 +0100

    feat: create a global style

--- swifty_company/styles/global.styles.js
+++ swifty_company/styles/global.styles.js
@@ -1,11 +1,31 @@
 import { StyleSheet } from "react-native";
 import { useMemo } from "react";
+import { TYPE_SIZES, TYPE_FONT_Famliy } from "./base.styles";
 
-const useStyle = (props) => ({
-
-    return useMemo(() => StyleSheet.create({
-        container: {
-            backgroundColor: red;
-        }
-    }), []);
-});
+const useStyle = (props) => {
+  return useMemo(
+    () =>
+      StyleSheet.create({
+        Paragraph: {
+          fontSize: TYPE_SIZES.FONT_SIZE_20,
+          fontFamily: TYPE_FONT_Famliy.POPPINS_REGULAR,
+          letterSpacing: 0.5,
+          lineHeight: 30,
+        },
+        Title: {
+          fontFamily: TYPE_FONT_Famliy.POPPINS_BOLD,
+          fontSize: TYPE_SIZES.FONT_SIZE_36,
+        },
+        Subtitle: {
+          fontFamily: TYPE_FONT_Famliy.POPPINS_EXTRA_BOLD,
+          fontSize: TYPE_SIZES.FONT_SIZE_36,
+        },
+        User_N_G_C: {
+          fontFamily: TYPE_FONT_Famliy.POPPINS_SEMI_BOLD,
+          fontSize: TYPE_SIZES.FONT_SIZE_24,
+        },
+      }),
+    []
+  );
+};
+export default useStyle;
