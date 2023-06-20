9588d88b717f3a73542d62e70f0f0c4b21726da4
Z-Akdim <zakaria.akdim37@gmail.com>
2023-06-20 18:55:00 +0100

    feat: add new font

--- swifty_company/App.js
+++ swifty_company/App.js
@@ -1,19 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet } from "react-native";
+import { TYPE_GRADIENTS } from "./styles/base.styles";
+import { LinearGradient } from "expo-linear-gradient";
+import Login from "./screens/Login";
+import AppLoading from "expo-app-loading";
+import { useFonts } from "expo-font";
+import Input from "./components/Input";
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    "Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("./assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("./assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Medium": require("./assets/fonts/Poppins-Medium.ttf"),
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
-    <View style={styles.container}>
-
-    </View>
+    <LinearGradient
+      colors={TYPE_GRADIENTS.LINEAR_GRADIENT_GREEN}
+      style={styles.container}
+    >
+      {/* <Login /> */}
+      <Input type="pandora" />
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
