88b2a97441e2333d7574a4008aa9fdeb931871ad
Z-Akdim <zakaria.akdim37@gmail.com>
2023-07-15 18:47:20 +0100

    feat: add new icon svg name logoTeam

---
+++ swifty_company/components/atoms/icons/logoTeam/index.jsx
@@ -0,0 +1,17 @@
+import { Svg, Path, Rect } from "react-native-svg";
+import { typeColor } from "../../../../styles/global.styles";
+const LogoTeam = (props) => {
+  const { width = 70, height = 78, type } = props;
+  const Colors = typeColor(type);
+  return (
+    <Svg width={width} height={height} viewBox="0 0 89 98" fill={Colors.title}>
+      <Rect x="6" width="76" height="61" fill={Colors.title} />
+      <Path
+        d="M43.7688 97.5888L6.21365 60.0048L82.0235 60.717L43.7688 97.5888Z"
+        fill={Colors.title}
+      />
+    </Svg>
+  );
+};
+
+export default LogoTeam;
