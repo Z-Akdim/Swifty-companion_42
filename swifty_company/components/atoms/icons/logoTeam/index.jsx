bbab1ef7510cbc504e92cb19aada7200b7a1a6d3
Z-Akdim <zakaria.akdim37@gmail.com>
2023-07-20 17:11:33 +0100

    change Colors.title to Colors.titleColor

--- swifty_company/components/atoms/icons/logoTeam/index.jsx
+++ swifty_company/components/atoms/icons/logoTeam/index.jsx
@@ -4,11 +4,16 @@ const LogoTeam = (props) => {
   const { width = 70, height = 78, type } = props;
   const Colors = typeColor(type);
   return (
-    <Svg width={width} height={height} viewBox="0 0 89 98" fill={Colors.title}>
-      <Rect x="6" width="76" height="61" fill={Colors.title} />
+    <Svg
+      width={width}
+      height={height}
+      viewBox="0 0 89 98"
+      fill={Colors.titleColor}
+    >
+      <Rect x="6" width="76" height="61" fill={Colors.titleColor} />
       <Path
         d="M43.7688 97.5888L6.21365 60.0048L82.0235 60.717L43.7688 97.5888Z"
-        fill={Colors.title}
+        fill={Colors.titleColor}
       />
     </Svg>
   );
