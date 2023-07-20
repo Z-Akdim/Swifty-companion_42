54990b8dd3f19681883933f8d5dd8b0dfd7cbdbd
Z-Akdim <zakaria.akdim37@gmail.com>
2023-07-20 18:02:13 +0100

    refactor: add new font size

--- swifty_company/styles/base.styles.js
+++ swifty_company/styles/base.styles.js
@@ -1,20 +1,20 @@
-export const TYPE_COLORS = {
-  Green: "#235A16",
-  primaryGreen: "#59AC57",
-  secondaryGreen: "#5B835A",
+// export const TYPE_COLORS = {
+//   Green: "#235A16",
+//   primaryGreen: "#59AC57",
+//   secondaryGreen: "#5B835A",
 
-  Blue: "#235A16",
-  primaryBlue: "#59AC57",
-  secondaryBlue: "#5B835A",
+//   Blue: "#235A16",
+//   primaryBlue: "#59AC57",
+//   secondaryBlue: "#5B835A",
 
-  Yellow: "#235A16",
-  primaryYellow: "#59AC57",
-  secondaryYellow: "#5B835A",
+//   Yellow: "#235A16",
+//   primaryYellow: "#59AC57",
+//   secondaryYellow: "#5B835A",
 
-  PinkColor: "#235A16",
-  primaryPinkColor: "#59AC57",
-  secondaryPinkColor: "#5B835A",
-};
+//   PinkColor: "#235A16",
+//   primaryPinkColor: "#59AC57",
+//   secondaryPinkColor: "#5B835A",
+// };
 
 export const TYPE_FONT_Famliy = {
   POPPINS_BOLD: "Poppins-Bold",
@@ -28,7 +28,9 @@ export const TYPE_SIZES = {
   FONT_SIZE_16: 16,
   FONT_SIZE_18: 18,
   FONT_SIZE_24: 24,
+  FONT_SIZE_20: 20,
   FONT_SIZE_30: 30,
+  FONT_SIZE_36: 36,
 };
 
 export const TYPE_PORDER_RADIUS = {
@@ -37,9 +39,9 @@ export const TYPE_PORDER_RADIUS = {
   BORDER_RADIUS_50: 50,
 };
 
-export const TYPE_GRADIENTS = {
-  LINEAR_GRADIENT_GREEN: ["#E5FFE2", "#CEFFCADE", "#E5FFE2"],
-  LINEAR_GRADIENT_BLUE: ["#E0FEFCE3", "#C8FFFCDE", "#E0FEFCE3"],
-  LINEAR_GRADIENT_YELLOW: ["#FEFFE1E3", "#FEFFC7DE", "#FEFFE1E3"],
-  LINEAR_GRADIENT_Pink: ["#FFE2F5E3", "#FFD0EFDE", "#FFE2F5E3"],
-};
+// export const TYPE_GRADIENTS = {
+//   commodore: {},
+//   bois: {},
+//   freax: {},
+//   pandora: {},
+// };
