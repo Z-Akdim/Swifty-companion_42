35180c2dbf4dadfdd5f5705c90e9031cbe9deed8
Z-Akdim <zakaria.akdim37@gmail.com>
2023-07-20 17:46:16 +0100

    refactor: change margin horizontal

--- swifty_company/components/Input/style.input.js
+++ swifty_company/components/Input/style.input.js
@@ -15,16 +15,17 @@ const useStyles = (type) => {
         inputContainerStyle: {
           borderStyle: "solid",
           borderWidth: 1,
-          marginHorizontal: 37,
           height: 60,
           borderRadius: TYPE_PORDER_RADIUS.BORDER_RADIUS_5,
           backgroundColor: Color.bgColor,
           borderColor: Color.borderColor,
+          marginHorizontal: 37,
         },
         inputStyle: {
           fontFamily: TYPE_FONT_Famliy.POPPINS_REGULAR,
           fontSize: TYPE_SIZES.FONT_SIZE_16,
           color: Color.placHoColor,
+          opacity: 0.81,
         },
 
         leftIconContainerStyle: { marginHorizontal: 7 },
